e3a4ee536bf5666b7b63b194613b628374144386
Kibeom Kim <terr1976@hotmail.com>
2016-01-21 15:32:46 -0500

    Remove debugger in article_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,6 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    debugger
     @article = Article.new(article_params)
     @article.user = current_user
     if @article.save
