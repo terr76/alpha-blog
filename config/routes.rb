8e5f1f2e07debb1660d7fc3ef4e1f83e60fb812b
Kibeom Kim <terr1976@hotmail.com>
2016-01-21 08:54:02 -0500

    Complete authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
   # post 'users', to: 'users#create'
   resources :users, excerpt: [:new]
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
